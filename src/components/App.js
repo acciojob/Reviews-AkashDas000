87ca2edcdb5266e069f25e6a083bb0aa7556e3d3
Akash Das <akashakki1100@gmail.com>
2025-10-07 15:10:12 +0530

    fist commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,102 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
 
+const App = () => {
+
+    const user = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+// const [data, setData] = useState(user)
+const [count, setCount] = useState(0)
+console.log(user)
+
+const handleNext = () => {
+    setCount((prev)=>{
+        return prev+1 === user.length ? 0 : prev + 1
+    })
+}
+
+const handlePrev = () => {
+    setCount((prev)=>{
+        return prev-1 < 0 ? user.length - 1 : prev-1
+    })
+}
+
+const handleRandom = () => {
+    let random = Math.floor(Math.random() * user.length);
+    if (random === count) {
+      random = (random + 1) % user.length;
+    }
+    setCount(random);
+
+}
+
+const {id, name, image, job, text} = user[count]
+
+ return (
+    <main>
+      <section className="container">
+        <h2 id="review-heading">Our Reviews</h2>
+
+        <article className="review">
+          <img width="100px" src={image} alt={name} className="person-img" />
+          <h4 className="author" id={`author-${id}`}>
+            {name}
+          </h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+
+          <div className="button-container">
+            <button className="prev-btn" onClick={handlePrev}>
+              Previous
+            </button>
+            <button className="next-btn" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+
+          <button className="random-btn" onClick={handleRandom}>
+            Surprise Me
+          </button>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default App
